648170ba48f97fc07ef202c95266d7672ccb0d08
cseadmin <cseadmin@OrchidCSEClient.nkocem.ac.in>
2019-04-04 15:30:41 +0530

    sample

---
+++ src/add_main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author cseadmin
+ */
+import arth.addition;
+public class add_main {
+    public static void main(String args[])
+    {
+        addition a= new addition(10,20);
+        a.area();
+    }
+}
---
+++ src/arth/addition.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arth;
+
+public class addition {
+    int l,b;
+    
+    public  addition(int a,int c)
+    {
+            l=a;
+            b=c;
+        
+    }
+    public void area()
+	{
+		int temp=l+b;
+		System.out.println("Addition is: "+temp);
+	}
+    
+}
